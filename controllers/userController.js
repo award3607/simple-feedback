2834040bc15b3b2b2cc73f93151a075d333e2a23
Andrew Ward <andrew.rampant@gmail.com>
2018-01-13 20:55:46 -0600

    saturday work

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,9 @@
 var User = require('../models/user.js');
+
+exports.login = (req, res) => {
+    res.render('login');
+}
+
+exports.user_create = (req, res) => {
+    res.render('create-user');
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var feedbackController = require('../controllers/feedbackController.js');
+var userController = require('../controllers/userController.js');
 
 var router = express.Router();
 
@@ -20,5 +21,9 @@ router.get('/', (req, res) => {
 router.get('/admin', feedbackController.feedback_admin);
 
 router.get('/test', feedbackController.feedback_test);
+
+router.get('/login', userController.login);
+
+router.get('/user', userController.user_create);
 
 module.exports = router;
